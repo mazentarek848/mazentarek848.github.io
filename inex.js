84ed64286b51215c4b776f00e9e469700353daa8
mazentarek848 <mazentarek415@gmail.com>
2026-09-17 20:29:43 +0300

    Remove theme toggle functionality from inex.js

--- inex.js
+++ inex.js
@@ -1,25 +1,4 @@
 document.addEventListener("contextmenu",function(e){
   e.preventDefault(e)
 })
-const themeBtn = document.getElementById("themeBtn");
-
-
-themeBtn.addEventListener("click", function () {
-
-    document.body.classList.toggle("light");
-
-
-    if (document.body.classList.contains("light")) {
-
-        themeBtn.innerHTML =
-            '<i class="fa-solid fa-sun"></i>';
-
-    } else {
-
-        themeBtn.innerHTML =
-            '<i class="fa-solid fa-moon"></i>';
-
-    }
-
-});
 
